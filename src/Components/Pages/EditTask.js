2209166b988e93c1b87eba626e0db26089f586d4
omarfaruk7022 <farhanfaruk221@gmail.com>
2022-10-18 17:08:38 +0600

    last

--- src/Components/Pages/EditTask.js
+++ src/Components/Pages/EditTask.js
@@ -62,7 +62,7 @@ const EditTask = (task) => {
                 // defaultValue={addingTask}
               />
 
-              <label class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-500 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+              <label class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-500 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 my-2">
                 Edit Task
               </label>
               <button type="submit">
--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -110,16 +110,16 @@ const Home = () => {
               className="dropdown-content shadow bg-base-100 rounded-box"
             >
               <form onSubmit={handleCorrect}>
-                <div class="relative z-0 mb-6 w-full group flex">
+                <div class="relative z-0 mb-6 w-full group flex ">
                   <input
                     type="text"
                     name="task"
-                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-green-500  peer"
+                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-green-500  peer  mx-5"
                     placeholder=" "
                     required
                   />
 
-                  <label class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-500 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                  <label class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-500 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 mx-5">
                     Add task
                   </label>
                   <button type="submit">
