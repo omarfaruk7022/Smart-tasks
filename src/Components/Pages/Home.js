cb7d90aeb02c27ea36e4b4ee31c78925822f5d81
omarfaruk7022 <farhanfaruk221@gmail.com>
2022-10-18 17:11:11 +0600

    last

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -119,7 +119,7 @@ const Home = () => {
                     required
                   />
 
-                  <label class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-500 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 mx-5">
+                  <label class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-500 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 mt-2 mx-5">
                     Add task
                   </label>
                   <button type="submit">
